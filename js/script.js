10ad117dfe175d7c494caadc1f1c58e402b3a889
Itzel Gil <itzel6276@gmail.com>
2021-04-19 20:09:43 +0200

    delete button + changed colors

--- js/script.js
+++ js/script.js
@@ -4,16 +4,21 @@ function onLoad(){
 
 
 var shoppingCart = document.getElementById("shoppingcart");
+var containerStyles = document.getElementById("container");
 var styles = document.getElementsByClassName("style");
 
+ 
 
+    //drag drop from styles to shopping cart//
     shoppingCart.addEventListener("dragover", (ev) => allowDrop(ev));
     shoppingCart.addEventListener("drop", (ev) => drop(ev));
-
+    //drag drop from shopping cart to styles//
+    containerStyles.addEventListener("dragover", (ev) => allowDrop(ev));
+    containerStyles.addEventListener("drop", (ev) => drop1(ev));
 
         for (var i = 0; i < styles.length; i++){
             styles[i].setAttribute("draggable", "true");
-            styles[i].setAttribute("id", "product"+i);
+            styles[i].setAttribute("id", "product" + i + "_" + styles[i].parentNode.id);
             styles[i] .addEventListener("dragstart", (ev) => startDrag(ev))
         }
     }
@@ -35,12 +40,32 @@ var styles = document.getElementsByClassName("style");
             var price = document.getElementById(data).alt;
             total = total + parseInt(price);
             document.getElementById("totalPrice").innerText = total.toString() + '€';
-            shoppingCart.appendChild(document.getElementById(data));
+            var deleteButton = document.createElement('button');
+            deleteButton.textContent = 'X';
+            deleteButton.id = "borrar_" + data; 
+            deleteButton.addEventListener('click', borrar);
+            var producto = document.getElementById(data);
+    
+            shoppingCart.appendChild(producto);
+            shoppingCart.appendChild(deleteButton);
             console.log(shoppingCart.childElementCount);
             
         }
-
-       // var deleteItem = document.createElement('button');
-       // deleteItem.classList.add('btn', 'btn-danger', 'mx-5');
-       // deleteItem.textContent = "X";
-       // deleteItem,addEventListener('click', deleteItemCart);        
+        
+        function borrar(ev){
+            ev.preventDefault();
+            var id = ev.currentTarget.id;
+            var split = id.split("_");
+            var data = document.getElementById(id);
+            var price = document.getElementById(split[1] + "_" + split[2]).alt;
+            var producto = document.getElementById(split[1] + "_" + split[2]);
+            total = total - parseInt(price);
+            document.getElementById("totalPrice").innerText = total.toString() + '€';
+            data.remove();
+            producto.remove();
+            var container = document.getElementById(split[2]);
+            data.id = split[1] + "_" + split[2];
+            container.appendChild(producto);
+        }
+        
+        
