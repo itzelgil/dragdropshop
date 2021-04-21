40198c5f9a067296e2e08933eab97a17d344811e
Itzel Gil <itzel6276@gmail.com>
2021-04-21 12:09:25 +0200

    changes on var

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var total = 0;
+let total = 0;
 
 function onLoad(){
 
 
-var shoppingCart = document.getElementById("shoppingcart");
-var containerStyles = document.getElementById("container");
-var styles = document.getElementsByClassName("style");
+let shoppingCart = document.getElementById("shoppingcart");
+let containerStyles = document.getElementById("container");
+let styles = document.getElementsByClassName("style");
 
  
 
@@ -16,7 +16,7 @@ var styles = document.getElementsByClassName("style");
     containerStyles.addEventListener("dragover", (ev) => allowDrop(ev));
     containerStyles.addEventListener("drop", (ev) => drop1(ev));
 
-        for (var i = 0; i < styles.length; i++){
+        for (let i = 0; i < styles.length; i++){
             styles[i].setAttribute("draggable", "true");
             styles[i].setAttribute("id", "product" + i + "_" + styles[i].parentNode.id);
             styles[i] .addEventListener("dragstart", (ev) => startDrag(ev))
@@ -34,17 +34,17 @@ var styles = document.getElementsByClassName("style");
         }
         //-- Drop function + total//
         function drop(ev){
-        var shoppingCart = document.getElementById("shoppingcart");
+        let shoppingCart = document.getElementById("shoppingcart");
             ev.preventDefault();
-            var data = ev.dataTransfer.getData("productid");
-            var price = document.getElementById(data).alt;
+            let data = ev.dataTransfer.getData("productid");
+            let price = document.getElementById(data).alt;
             total = total + parseInt(price);
             document.getElementById("totalPrice").innerText = total.toString() + '€';
-            var deleteButton = document.createElement('button');
+            let deleteButton = document.createElement('button');
             deleteButton.textContent = 'X';
             deleteButton.id = "borrar_" + data; 
             deleteButton.addEventListener('click', borrar);
-            var producto = document.getElementById(data);
+            let producto = document.getElementById(data);
     
             shoppingCart.appendChild(producto);
             shoppingCart.appendChild(deleteButton);
@@ -54,16 +54,16 @@ var styles = document.getElementsByClassName("style");
         //--Delete function
         function borrar(ev){
             ev.preventDefault();
-            var id = ev.currentTarget.id;
-            var split = id.split("_");
-            var data = document.getElementById(id);
-            var price = document.getElementById(split[1] + "_" + split[2]).alt;
-            var producto = document.getElementById(split[1] + "_" + split[2]);
+            let id = ev.currentTarget.id;
+            let split = id.split("_");
+            let data = document.getElementById(id);
+            let price = document.getElementById(split[1] + "_" + split[2]).alt;
+            let producto = document.getElementById(split[1] + "_" + split[2]);
             total = total - parseInt(price);
             document.getElementById("totalPrice").innerText = total.toString() + '€';
             data.remove();
             producto.remove();
-            var container = document.getElementById(split[2]);
+            let container = document.getElementById(split[2]);
             data.id = split[1] + "_" + split[2];
             container.appendChild(producto);
         }
